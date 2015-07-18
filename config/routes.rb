95efcae3481c94f6f65d043b9d448cc214a69e8a
Olly Wilson <ollywilson@gmail.com>
2015-07-18 11:58:17 +0000

    built out navigation and added welcome controller and set route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/features'
+
   resources :invoices
-  root to: 'invoices#index'
+  root to: 'welcome#index'
 end
